ef57ff4f7783fea6c73f9e33a11705bb07160726
John Salako <salakojo@msu.edu>
2024-11-04 02:44:53 -0500

    data

--- stab_zone.py
+++ stab_zone.py
@@ -1,11 +1,12 @@
 import ee
-import json
 import datetime
+import geemap
+import xarray
+import pycrs
+import rasterio
+import localtileserver
 import streamlit as st
-import geopandas as gpd
 import geemap.foliumap as geemap
-import geopandas as gpd
-import geemap as geemap_r
 
 # setting webpage title and icon
 st.set_page_config(page_title="Dynamic Yield Stability Map", page_icon='🛰️', layout='wide')
