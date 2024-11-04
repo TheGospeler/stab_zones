d534d651edcedc41ea02a48fd055433a19463b81
John Salako <salakojo@msu.edu>
2024-11-04 09:59:03 -0500

    data

--- stab_zone.py
+++ stab_zone.py
@@ -1,6 +1,7 @@
 import ee
 import datetime
 import geemap
+import json
 import xarray
 import pycrs
 import rasterio
@@ -12,6 +13,38 @@ import geemap.foliumap as geemap
 st.set_page_config(page_title="Dynamic Yield Stability Map", page_icon='🛰️', layout='wide')
 
 st.title("Interactive Raster Visualization")
+st.write("""
+This website provides a dynamic visualization of the yield stability map (YSM). The sliding window also enables
+         the comparison of any of the yield stability map created by the different satellite to visualize the 
+         effects of the spatial resolution on the yield stability map.
+""")
+
+# initializing earth engine credentials
+json_data = st.secrets["json_data"]
+service_account = st.secrets["service_account"]
+
+json_object = json.loads(json_data, strict=False)
+json_object = json.dumps(json_object)
+credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
+ee.Initialize(credentials)
+
+left, right = st.columns(2)  # month, year column
+with left:
+  st.write('##### Left-side Visualization:')
+  feature_l = st.selectbox('Select Feature / Raster:', ['Landsat', 'Sentinel', 'Planet', 'Field Boundary'], key='option_1')
+
+with right:
+  st.write('##### Right-side Visualization:')
+  feature_r = st.selectbox('Select Feature / Raster:', ['Sentinel', 'Landsat', 'Planet', 'Field Boundary'], key='option_2')
+
+# creating a dictionary that stores that holds the location of the feature/ Raster
+
+info_dict = {
+  "Landsat": r"data/rasters/Landsat.tif",
+  "Sentinel": r"data/rasters/Sentinel.tif",
+  "Planet": r"data/rasters/Planet.tif",
+  "Field Boundary": geemap.shp_to_ee(r"data/shapefile/Field123.shp")
+}
 
 # read important file 
 aoi_shp = r"data/shapefile/Field123.shp"
@@ -21,8 +54,16 @@ planet = r"data/rasters/Planet.tif"
 # Initialize Geemap map
 map = geemap.Map(center=[43.582, -84.733], zoom=15, height=600)
 map.add_basemap('HYBRID')
-map.addLayer(region, {}, "Field Boundaries")
-map.add_raster(planet)
+if feature_l == 'Field Boundaries':
+    map.addLayer(info_dict[feature_l], {}, "Field Boundaries")
+    map.add_raster(info_dict[feature_r], layer_name=feature_l)
+
+elif feature_r == 'Field Boundaries':
+    map.addLayer(info_dict[feature_r], {}, "Field Boundaries")
+    map.add_raster(info_dict[feature_r], layer_name=feature_r)
+
+else:
+    map.add_raster(planet)
 
 # Display map
 map.to_streamlit(height=600)
